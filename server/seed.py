eef752bc96ed20d2dbc4f9b3b1cff026225d625d
collins kipngeno <kipngenolins01@gmail.com>
2024-07-27 03:21:50 +0300

    completed

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,19 @@
-#!/usr/bin/env python3
-
-from app import app
-from models import db, Customer, Review, Item
+from server import app
+from server.models import db, Customer, Item, Review
 
 with app.app_context():
-
-    Customer.query.delete()
-    Review.query.delete()
-    Item.query.delete()
-
+    # Add sample customers
     customer1 = Customer(name='Tal Yuri')
-    customer2 = Customer(name='Raha Rosario')
-    customer3 = Customer(name='Luca Mahan')
-    db.session.add_all([customer1, customer2, customer3])
-    db.session.commit()
+    customer2 = Customer(name='Dana Scully')
 
+    # Add sample items
     item1 = Item(name='Laptop Backpack', price=49.99)
     item2 = Item(name='Insulated Coffee Mug', price=9.99)
-    item3 = Item(name='6 Foot HDMI Cable', price=12.99)
-    db.session.add_all([item1, item2, item3])
-    db.session.commit()
 
-    db.session.add(Review(comment="zipper broke the first week",
-                   customer=customer1, item=item1))
-    db.session.add(Review(comment="love this backpack!",
-                   customer=customer2, item=item1))
-    db.session.add(Review(comment="coffee stays hot for hours!",
-                   customer=customer1, item=item2))
-    db.session.add(Review(comment="best coffee mug ever!",
-                   customer=customer3, item=item2))
-    db.session.add(Review(comment="cable too short",
-                   customer=customer3, item=item3))
+    # Add sample reviews
+    review1 = Review(comment='Great backpack!', customer=customer1, item=item1)
+    review2 = Review(comment='Keeps my coffee hot!', customer=customer1, item=item2)
+    review3 = Review(comment='Stylish and spacious.', customer=customer2, item=item1)
+
+    db.session.add_all([customer1, customer2, item1, item2, review1, review2, review3])
     db.session.commit()
